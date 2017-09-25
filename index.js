87c0a4c72bbfae19cdf8203cc06a4a4502e3c96d
James Royer <walkingalchemy@gmail.com>
2017-09-25 17:01:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function theBeatlesPlay(musicians, instruments) {
+  var plays = []
+  for (let i = 0; i < musicians.length; i++) {
+    plays.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return plays
+}
+
+function johnLennonFacts(facts) {
+  let countdown = facts.length
+  var jLFacts = []
+  while (countdown > 0) {
+    jLFacts.unshift(`${facts[countdown]}!!!`)
+  }
+  return jLFacts
+}
